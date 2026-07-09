1c673f5dea1df4421969e8ca705bb37d4c9f2aa9
Niklazbbb <niklasweber13372@gmail.com>
2026-07-09 20:38:10 +0200

    add first screen

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Wenn du diesen Text lesen kannst, hat alles funktioniert!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
